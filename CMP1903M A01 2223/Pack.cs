7259f828768a5d664936d84c8d8da291a2ebebd8
BSpoones <ben.soones@gmail.com>
2023-03-07 21:17:31 +0000

    Populated functions

--- CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/Pack.cs
@@ -3,31 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CMP1903M_A01_2223.CardShuffle;
 
 namespace CMP1903M_A01_2223
 {
     class Pack
     {
-        List<Card> pack;
+        public static List<Card> cardPack = makePack();
 
-        public Pack()
+        public bool shuffleCardPack(ShuffleType shuffleType)
         {
-            //Initialise the card pack here
+            switch (shuffleType)
+            {
+                case ShuffleType.FISHERYATES:
+                    cardPack = fisherShuffle(cardPack);
+                    break;
+                case ShuffleType.RIFFLE:
+                    cardPack = riffleShuffle(cardPack);
+                    break;
+                case ShuffleType.NONE:
+                    // Do nothing since cards is already unshuffled
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Invalid input");
+            }
+            return true;
         }
-
-        public static bool shuffleCardPack(int typeOfShuffle)
+        public Card deal()
         {
-            //Shuffles the pack based on the type of shuffle
+            /*
+            * Deals one card from the top of the deck
+            */
 
-        }
-        public static Card deal()
-        {
-            //Deals one card
-
+            if (cardPack.Count < 1)
+            {
+                throw new ArgumentOutOfRangeException("No cards left to deal");
+            }
+            Card dealtCard = cardPack.First();
+            // Dealing a card means it is removed from the pack
+            cardPack.Remove(dealtCard);
+            return dealtCard;
         }
         public static List<Card> dealCard(int amount)
         {
             //Deals the number of cards specified by 'amount'
+            return cardPack.GetRange(0, Math.Min(amount, cardPack.Count()));
+        }
+
+        public static List<Card> makePack()
+        {
+            List<Card> cardPack = new List<Card>();
+            // Creating an unsorted card pack
+            for (int i = 1; i <= 4; i++)
+            {
+                // Each card suit
+                for (int j = 1; j <= 13; j++)
+                {
+                    // Each card value
+                    // Console.WriteLine(i.ToString() + " " + j.ToString());
+                    cardPack.Add(new Card(i, j));
+                }
+            }
+            return cardPack;
+        }
+        public void outputPack()
+        {
+            foreach (Card card in cardPack)
+            {
+                Console.WriteLine(card.ToString());
+            }
         }
     }
+    
 }
