006395a406906c1ab594fb6e5e4b1f0c85c72d50
BSpoones <ben.soones@gmail.com>
2023-03-07 21:17:38 +0000

    Added basic testing

---
+++ CMP1903M A01 2223/Testing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CMP1903M_A01_2223.CardShuffle;
+
+namespace CMP1903M_A01_2223
+{
+    public class Testing
+    {
+        public static void Test()
+        {
+            Pack pack = new Pack();
+            pack.outputPack();
+            pack.shuffleCardPack(ShuffleType.FISHERYATES);
+            Console.WriteLine("Shuffling");
+            pack.outputPack();
+
+            Console.WriteLine("Dealing");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(pack.deal().ToString());
+            }
+        }
+    }
+}
